72cefd6ebe4c67b3d7d5f7cf9ad742ba6391b26e
lipecorradini <luizfelipecorradini@gmail.com>
2024-07-11 18:21:34 -0300

    fixing tensorflow wrong importing

--- examples/FakeNews.py
+++ examples/FakeNews.py
@@ -1,9 +1,12 @@
 import sys
 sys.path.insert(1, '../src')
 
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import LLM_MRI
 import matplotlib.pyplot as plt
-import os
 from datasets import load_from_disk
 import networkx as nx
 
