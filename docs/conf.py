25e06240be9eefe63de9375e8af519f52c06b5a9
lipecorradini <luizfelipecorradini@gmail.com>
2024-07-11 20:52:29 -0300

    fixing sphinx name bug

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ release = '01.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["shinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
