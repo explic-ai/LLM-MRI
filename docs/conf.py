8ff06903ff1e7d1d0a73e9947ca713d186a9df74
lipecorradini <luizfelipecorradini@gmail.com>
2024-07-12 16:55:24 -0300

    test classic

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,4 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os
 import sys
@@ -16,31 +10,20 @@ copyright = '2024, Luiz Costa, Mateus Figenio, André Santanchè, Luiz Gomes-Jr'
 author = 'Luiz Costa, Mateus Figenio, André Santanchè, Luiz Gomes-Jr'
 release = '01.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
-    "sphinx.ext.todo", 
-    "sphinx.ext.viewcode", 
-    "sphinx.ext.autodoc",
-    "sphinx_rtd_theme"
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc"
 ]
+
+html_theme = "classic"  # Changed to a built-in theme
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme'  # Changed theme to sphinx_rtd_theme
-
-# Optional: Add theme options
-html_theme_options = {
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
-
 html_static_path = ['_static']
+
+# Optional: Add theme options (specific to the theme you choose)
+html_theme_options = {
+    'rightsidebar': False,
+    'relbarbgcolor': 'black'
+}
